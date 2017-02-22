a8353a0dc2715fdcad4661fdcc15efe474833088
cookkkie <cookie.io@openmailbox.org>
2017-02-22 03:28:31 +0100

    Music port to dpy

--- chat-bot/plugins/music.py
+++ chat-bot/plugins/music.py
@@ -1,32 +1,250 @@
-from plugin import Plugin
+import asyncio
+import aiohttp
+import os
+import json
+import functools
+import discord
+from sys import platform
 
+from plugin import Plugin
+from decorators import command
+
+if not discord.opus.is_loaded():
+    if platform == "linux" or platform == "linux2":
+        discord.opus.load_opus('libopus.so')
+    elif platform == "darwin":
+        discord.opus.load_opus('libopus.dylib')
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 class Music(Plugin):
 
     fancy_name = "Music"
+    buff_name = "music"
+    players = dict()
+    call_next = True
 
-    async def get_commands(self, server):
-        commands = [
-            {
-                'name': '!join',
-                'description': 'Makes me join your voice channel'
-            },
-            {
-                'name': '!add music_name',
-                'description': 'Adds a music to the queue'
-            },
-            {
-                'name': '!playlist',
-                'description': 'Shows the music queue'
-            },
-            {
-                'name': '!stop',
-                'description': 'Stops the music'
-            },
-            {
-                'name': '!next',
-                'description': 'Goes to the next song'
-            }
-        ]
-        return commands
+    @command(pattern='^!play$',
+             require_one_of_roles="allowed_roles",
+             description="Makes me play the next song in the queue",
+             usage='!play')
+    async def play(self, m, args):
+        music = await self.pop_music(m.server)
+        if not music:
+            response = 'Nothing to play... :grimacing:'
+            return await self.mee6.send_message(m.channel, response)
 
+        voice = m.server.voice_client
+        if not voice:
+            response = "I'm not connected to any voice channel :grimacing:..."
+            return await self.mee6.send_message(m.channel, response)
+
+        try:
+            await self._play(m.server, music)
+        except Exception as e:
+            response = 'An error occurred, sorry :grimacing:...'
+            print(e)
+            await self.mee6.send_message(m.channel, response)
+
+    @command(pattern='^!next$',
+             description="Makes me jump to the next song in the queue",
+             require_one_of_roles="allowed_roles",
+             usage='!next')
+    async def next(self, m, args):
+        music = await self.pop_music(m.server)
+        if not music:
+            response = 'Nothing to play... :grimacing:'
+            return await self.mee6.send_message(m.channel, response)
+
+        voice = m.server.voice_client
+        if not voice:
+            response = "I'm not connected to any voice channel :grimacing:..."
+            return await self.mee6.send_message(m.channel, response)
+
+        try:
+            await self._play(m.server, music)
+        except Exception as e:
+            response = 'An error occurred, sorry :grimacing:...'
+            print(e)
+            await self.mee6.send_message(m.channel, response)
+
+    @command(pattern='^!stop$',
+             description="Makes me stop playing music",
+             require_one_of_roles="allowed_roles",
+             usage='!stop')
+    async def stop(self, m, args):
+        curr_player = self.players.get(m.server.id)
+        if curr_player:
+            self.call_next = False
+            curr_player.stop()
+
+    async def _next(self, guild):
+        music = await self.pop_music(guild)
+        if not music:
+            return
+
+        voice = guild.voice_client
+        if not voice:
+            return
+
+        try:
+            await self._play(guild, music)
+        except Exception as e:
+            response = 'An error occurred, sorry :grimacing:...'
+            print(e)
+            print(response)
+
+    def sync_next(self, guild):
+        def n():
+            if self.call_next:
+                self.mee6.loop.create_task(self._next(guild))
+            self.call_next = True
+        return n
+
+    async def _play(self, guild, music):
+        voice = guild.voice_client
+        opts = {
+         'default_search': 'auto',
+         'quiet': True,
+        }
+
+        curr_player = self.players.get(guild.id)
+        if curr_player:
+            curr_player.stop()
+
+        await self.set_np(music, guild)
+
+        player = await voice.create_ytdl_player(music['url'],
+                                                ytdl_options=opts,
+                                                after=self.sync_next(guild))
+        self.players[guild.id] = player
+        player.volume = 0.6
+        player.start()
+
+    @command(pattern='^!join',
+             description="Makes me join your current voice channel",
+             require_one_of_roles="allowed_roles",
+             usage='!join')
+    async def join(self, message, args):
+        voice_channel = message.author.voice.voice_channel
+        if not voice_channel:
+            response = "You are not in a voice channel."
+            return await self.mee6.send_message(message.channel, response)
+
+        voice = await self.mee6.join_voice_channel(voice_channel)
+        response = "Connecting to voice channel **{}**".format(voice_channel.name)
+        await self.mee6.send_message(message.channel, response)
+
+    @command(pattern='^!playlist$',
+             description="Shows the songs in the playlist",
+             require_one_of_roles="allowed_roles",
+             usage='!playlist')
+    async def playlist(self, message, args):
+        response = ""
+        storage = await self.get_storage(message.server)
+        now_playing = await storage.get('now_playing')
+        if now_playing:
+            now_playing = json.loads(now_playing)
+            np_fmt = "`NOW PLAYING` :notes: **{}** added by **{}**\n\n"
+            response += np_fmt.format(now_playing.get('title'),
+                                      now_playing['addedBy']['name'])
+
+        queue = await storage.lrange('request_queue', 0, 5)
+        for i, music_str in enumerate(queue[:5]):
+            music = json.loads(music_str)
+            fmt = "`#{}` **{}** added by **{}**\n"
+            response += fmt.format(i + 1, music.get('title'),
+                                   music['addedBy']['name'])
+
+        fmt = "\n `Full playlist ` <https://mee6.xyz/request_playlist/{}>"
+        response += fmt.format(message.server.id)
+
+        await self.mee6.send_message(message.channel, response)
+
+    @command(pattern='^!add (.*)',
+             description="Adds a new song in the queue",
+             require_one_of_roles="allowed_roles",
+             usage='!add name_of_the_music')
+    async def add(self, message, args):
+        search = args[0]
+
+        if 'http' in search:
+            video_url = search
+        else:
+            try:
+                video_url = await self.get_yt_video_url(search)
+            except Exception as e:
+                response = "Didn't find any video :cry:!"
+                return await self.mee6.send_message(message.channel,
+                                                    response)
+
+        try:
+            info = await self.get_audio_info(video_url)
+        except Exception as e:
+            response = "An error occured, sorry :cry:..."
+            return await self.mee6.send_message(message.channel,
+                                                response)
+
+        music = {"url": video_url,
+                 "title": info.get('title', ''),
+                 "thumbnail": ""}
+        music["addedBy"] = {"name": message.author.name,
+                            "discriminator": message.author.discriminator,
+                            "avatar": message.author.avatar_url}
+
+        await self.push_music(music, message.server)
+
+        response = "**{}** added! :ok_hand:".format(music["title"])
+        await self.mee6.send_message(message.channel, response)
+
+    async def get_yt_video_url(self, search):
+        url = "https://www.googleapis.com/youtube/v3/search"
+        with aiohttp.ClientSession() as session:
+            async with session.get(url, params={"type": "video",
+                                                "q": search,
+                                                "part": "snippet",
+                                                "key": GOOGLE_API_KEY}) as resp:
+                data = await resp.json()
+
+        if not data.get("items"):
+            raise Exception("An error occured")
+
+        items = data["items"]
+        if len(items) == 0:
+            return None
+
+        video_url = "https://youtube.com/watch?v=" + items[0]["id"]["videoId"]
+        return video_url
+
+    async def get_audio_info(self, url):
+        import youtube_dl
+
+        opts = {
+            'format': 'webm[abr>0]/bestaudio/best',
+            'prefer_ffmpeg': True
+        }
+
+        ydl = youtube_dl.YoutubeDL(opts)
+        func = functools.partial(ydl.extract_info, url, download=False)
+        info = await self.mee6.loop.run_in_executor(None, func)
+        if "entries" in info:
+            info = info['entries'][0]
+
+        return info
+
+    async def push_music(self, music, guild):
+        storage = await self.get_storage(guild)
+        return await storage.rpush("request_queue", json.dumps(music))
+
+    async def set_np(self, music, guild):
+        storage = await self.get_storage(guild)
+        return await storage.set("now_playing", json.dumps(music))
+
+    async def pop_music(self, guild):
+        storage = await self.get_storage(guild)
+        music_str = await storage.lpop("request_queue")
+
+        if not music_str:
+            return None
+
+        return json.loads(music_str)
