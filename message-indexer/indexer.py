865a013c41f76bc8aa471231134ca0c43d843a62
cookkkie <cookie.io@openmailbox.org>
2017-02-24 17:28:00 +0100

    Removed indexing

--- message-indexer/indexer.py
+++ message-indexer/indexer.py
@@ -9,7 +9,7 @@ REDIS_URL = os.getenv('REDIS_URL')
 ES_URL = os.getenv('ES_URL')
 COUNT = 5000
 
-es = Elasticsearch([ES_URL])
+#es = Elasticsearch([ES_URL])
 r = redis.from_url(REDIS_URL, decode_responses=True)
 
 i = 1
@@ -29,8 +29,8 @@ while True:
     raw_message = payload[1]
     message = json.loads(raw_message)
     message['timestamp'] = now
-    es.index(doc_type='message',
-             id=int(message['id']),
-             body=message,
-             index='message')
+    #es.index(doc_type='message',
+    #         id=int(message['id']),
+    #         body=message,
+    #         index='message')
     i += 1
