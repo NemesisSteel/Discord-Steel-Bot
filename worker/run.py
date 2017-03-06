2aa76691ee47beaaaef5212c67b9ef2c73600bab
cookkkie <cookie.io@openmailbox.org>
2017-03-06 19:33:17 +0100

    Remove loading of some plugins

--- worker/run.py
+++ worker/run.py
@@ -1,6 +1,10 @@
 from worker_bot import WorkerBot
 from plugins.printer import Printer
+from plugins.welcome import Welcome
+from plugins.indexer import Indexer
 
-bot = WorkerBot()
-bot.load_plugin(Printer)
+import os
+
+bot = WorkerBot(discord_token=os.getenv('MEE6_TOKEN'))
+bot.load_plugin(Indexer)
 bot.run()
