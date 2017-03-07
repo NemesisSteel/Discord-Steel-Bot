d1256d251db7c869cd4fb2049495ff1263d991e7
cookkkie <cookie.io@openmailbox.org>
2017-03-07 19:20:45 +0100

    Add rpc functions

--- gateway/gateway_bot.py
+++ gateway/gateway_bot.py
@@ -1,5 +1,6 @@
 import discord
 import aioredis
+import inspect
 import asyncio
 import os
 import re
@@ -9,6 +10,13 @@ import logging
 
 from utils import dump
 from time import time
+from aiohttp import web
+
+if not discord.opus.is_loaded():
+    if platform == 'linux' or platform == 'linux2':
+        discord.opus.load_opus('./libopus.so')
+    elif platform == 'darwin':
+        discord.opus.load_opus('libopus.dylib')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,7 +29,38 @@ def parse_redis_url(redis_url):
 
 	return (result[0], 6379)
 
+class RPCException(Exception):
+    pass
+
+def make_handler(func):
+    async def handler(request):
+        args_list = [request.match_info[arg] for arg in \
+                     func.rpc_info['args']]
+        try:
+            if inspect.iscoroutinefunction(func):
+                resp = await func(*args_list)
+            else:
+                resp = func(*args_list)
+        except RPCException as e:
+            return web.json_response({'error': str(e)}, status=500)
+        print(resp)
+        return web.json_response({'msg': 'ok'})
+    return handler
+
+def rpc(method):
+    args = list(inspect.signature(method).parameters)[1:]
+    method_name = method.__name__
+
+    method.is_rpc = True
+    print(args)
+    method.rpc_info = {'name': method_name,
+                       'args': args}
+    return method
+
 class GatewayBot(discord.Client):
+
+    players = dict()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -34,6 +73,35 @@ class GatewayBot(discord.Client):
         self.gateway_repr = 'gateway-{}-{}'.format(self.shard_id,
                                                    self.shard_count)
         self.log = logging.getLogger(self.gateway_repr).info
+
+        self.rpc_app = web.Application()
+        self.rpc_app.router.add_get('/', self.rpc_hello)
+        self.register_rpcs()
+
+    def register_rpcs(self):
+        attrs = map(lambda attr_name: getattr(self, attr_name), dir(self))
+        methods = filter(inspect.ismethod, attrs)
+        rpcs = filter(lambda m: hasattr(m, 'is_rpc'), methods)
+        for rpc in rpcs:
+            info = rpc.rpc_info
+            path = '/{}'.format(info['name'])
+            if len(info['args']):
+                path += '/'
+            path += '/'.join(map(lambda a: '{' + a + '}', info['args']))
+            print(path)
+            self.rpc_app.router.add_get(path, make_handler(rpc))
+
+    def run_rpc_server(self):
+        handler = self.rpc_app.make_handler()
+        srv = self.loop.create_server(handler, '0.0.0.0', 8080)
+        self.loop.create_task(srv)
+        self.log('RPC serving on 0.0.0.0:8080')
+
+    def rpc_hello(self, request):
+        rsp = {'id': self.gateway_repr,
+               'guilds_count': len(self.servers)}
+        self.log(request.match_info)
+        return web.json_response(rsp)
 
     def redis_connect(self):
         loop = asyncio.get_event_loop()
@@ -58,10 +126,6 @@ class GatewayBot(discord.Client):
     async def send(self, queue, data):
         payload = json.dumps(data)
         await self.broker.lpush(queue, payload)
-
-    async def on_ready(self):
-        self.log('Connected to {} guilds'.format(len(self.servers)))
-        self.loop.create_task(self.ping())
 
     async def ping(self):
         PING_INTERVAL = 10
@@ -136,6 +200,9 @@ class GatewayBot(discord.Client):
                                        after)
 
     async def on_ready(self):
+        self.log('Connected to {} guilds'.format(len(self.servers)))
+        self.run_rpc_server()
+        self.loop.create_task(self.ping())
         for server in list(self.servers):
             self.loop.create_task(self.on_server_ready(server))
 
@@ -166,3 +233,95 @@ class GatewayBot(discord.Client):
         await self.send_dispatch_event('MEMBER_REMOVE',
                                        member.server,
                                        member)
+
+    @rpc
+    def get_server(self, server_id):
+        server = discord.utils.get(self.servers, id=server_id)
+        if not server:
+            raise RPCException('Guild not found')
+
+        return server
+
+    @rpc
+    def get_voice_channel(self, server, voice_channel_id):
+        print(server, voice_channel_id)
+        if type(server) in (str, int):
+            server = self.get_server(server)
+
+        voice_channel = discord.utils.get(server.channels, id=str(voice_channel_id))
+        if not voice_channel:
+            raise RPCException('Voice channel not found')
+
+        if voice_channel.type != discord.ChannelType.voice:
+            raise RPCException('This is not a voice channel')
+
+        return voice_channel
+
+    @rpc
+    async def join_voice(self, server, voice_channel_id):
+        if type(server) in (str, int):
+            server = self.get_server(server)
+
+        voice_channel = self.get_voice_channel(server, voice_channel_id)
+
+        voice = server.voice_client
+        if voice:
+            await voice.move_to(voice_channel)
+        else:
+            await self.join_voice_channel(voice_channel)
+
+        return voice_channel
+
+    @rpc
+    async def leave(self, server):
+        if type(server) in (str, int):
+            server = self.get_server(server)
+
+        voice = server.voice_client
+        if voice:
+            await voice.disconnect()
+
+        return None
+
+    @rpc
+    def get_voice_client(self, server):
+        if type(server) in (str, int):
+            print(server)
+            print(type(server))
+            server = self.get_server(server)
+
+        voice = server.voice_client
+        print(voice)
+        if not voice:
+            raise RPCException('Not connected to voice')
+
+        return voice
+
+    @rpc
+    async def ytdl_play_song(self, server, song_url):
+        if type(server) in (str, int):
+            print(server)
+            print(type(server))
+            server = self.get_server(server)
+            print(server)
+
+        voice = self.get_voice_client(server)
+
+        lock = self.play_locs[guild.id]
+        await lock.acquire()
+
+        curr_player = self.players.get(server.id)
+        if curr_player:
+            self.call_next[server.id] = False
+            curr_player.stop()
+
+        player = await voice.create_ytdl_player(url,
+                                                ytdl_options=opts)
+
+        self.players[guild.id] = player
+        player.volume = 0.6
+        player.start()
+
+        lock.release()
+
+        return voice
