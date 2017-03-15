0da80ca35763ce8b7a2313703b1cd2b27ee8d32f
cookkkie <cookie.io@openmailbox.org>
2017-03-15 23:15:42 +0100

    Add config module

---
+++ worker/config.py
@@ -0,0 +1,10 @@
+import os
+
+BROKER_URL = os.getenv('BROKER_URL')
+REDIS_URL = os.getenv('REDIS_URL')
+IMGUR_ID = os.getenv('IMGUR_ID')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+MEE6_TOKEN = os.getenv('MEE6_TOKEN')
+MAL_USERNAME = os.getenv('MAL_USERNAME')
+MAL_PASSWORD = os.getenv('MAL_PASSWORD')
