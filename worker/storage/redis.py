aa6c6066b79bb95065315f0ebe09f3a0a947df27
cookkkie <cookie.io@openmailbox.org>
2017-03-17 19:06:34 +0100

    Move redis storage

---
+++ worker/storage/redis.py
@@ -0,0 +1,24 @@
+class RedisStorage:
+    def __init__(self, guild_id, plugin_name, redis):
+        self.guild_id = guild_id
+        self.plugin_name = plugin_name
+
+        self.prefix = '{}.{}:'.format(plugin_name,
+                                      guild_id)
+        self.redis = redis
+
+    def set(self, key, value):
+        key = self.prefix + key
+        return self.redis.set(key, value)
+
+    def get(self, key):
+        key = self.prefix + key
+        return self.redis.get(key)
+
+    def smembers(self, key):
+        key = self.prefix + key
+        return self.redis.smembers(key)
+
+    def sadd(self, key, member, *members):
+        key = self.prefix + key
+        return self.redis.sadd(key, member, *members)
