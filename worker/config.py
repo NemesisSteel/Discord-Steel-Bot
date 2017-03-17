1d30b834ec6583d933f7d8002e24c4b6f9d10a89
cookkkie <cookie.io@openmailbox.org>
2017-03-17 19:03:45 +0100

    Add POSTGRES config to config.py

--- worker/config.py
+++ worker/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 BROKER_URL = os.getenv('BROKER_URL')
 REDIS_URL = os.getenv('REDIS_URL')
 IMGUR_ID = os.getenv('IMGUR_ID')
@@ -8,3 +9,9 @@ TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
 MEE6_TOKEN = os.getenv('MEE6_TOKEN')
 MAL_USERNAME = os.getenv('MAL_USERNAME')
 MAL_PASSWORD = os.getenv('MAL_PASSWORD')
+
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = os.getenv('POSTGRES_USER')
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'mee6')
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'mee6')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'mee6')
